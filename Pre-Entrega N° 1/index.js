c123292df11216a49146263543e1d74d6afa9592
agusbruzzi <agusb.bruzzi@gmail.com>
2022-09-01 05:18:37 -0300

    primera entrega, file sistem

--- Pre-Entrega N° 1/index.js
+++ Pre-Entrega N° 1/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const { Router } = express;
 const app = express();
@@ -17,50 +18,35 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const administrador = true;
 
-let products = [
-  {
-    id: 1,
-    timestamp: 1662007056984,
-    nombre: "remera",
-    descripcion: "descripción del producto",
-    codigo: "FYr3VGWDmyDnKKVVftG9",
-    precio: 500,
-    foto: "https://deliverind.com.ar/wp-content/uploads/2022/06/REMERA-REGULAR-FIT-SOFT-22-PORTADA-01-scaled.jpg",
-    stock: 5,
-  },
-  {
-    id: 2,
-    timestamp: 1662007058984,
-    nombre: "pantalon",
-    descripcion: "descripción del producto",
-    codigo: "wD2mPyXZW6R46nKgp9dJ",
-    precio: 800,
-    foto: "https://deliverind.com.ar/wp-content/uploads/2020/11/UNIDAD-71-scaled.jpg",
-    stock: 3,
-  },
-  {
-    id: 3,
-    timestamp: 1662007058284,
-    nombre: "medias",
-    descripcion: "descripción del producto",
-    codigo: "62BPB72hk2jgGvM2ZM7r",
-    precio: 200,
-    foto: "https://deliverind.com.ar/wp-content/uploads/2021/09/4-10-scaled.jpg",
-    stock: 6,
-  },
-];
-
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log("Servidor encendido");
 });
 
+function read(archivo) {
+  try {
+    return (contenido = JSON.parse(fs.readFileSync(archivo, "utf-8")));
+  } catch (error) {
+    console.log("Error en lectura. Detalle del error: ", error);
+  }
+}
+
+function write(archivo, contenido) {
+  try {
+    return (contenido = fs.writeFileSync(archivo, JSON.stringify(contenido)));
+  } catch (error) {
+    console.log("Error en escritura. Detalle del error: ", error);
+  }
+}
+
 routerProducts.get("/", (req, res) => {
+  const products = read("./productos.json");
   res.json(products);
 });
 
 routerProducts.get("/:id", (req, res) => {
   const { id } = req.params;
-  product = products.find((x) => x.id == id);
+  const products = read("./productos.json");
+  const product = products.find((x) => x.id == id);
   if (product) {
     res.json(product);
   } else {
@@ -82,6 +68,7 @@ routerProducts.post(
   jsonParser,
   (req, res) => {
     const { body } = req;
+    const products = read("./productos.json");
     //Asigno ID
     let lastID = products[products.length - 1].id;
     body.id = lastID + 1;
@@ -99,6 +86,7 @@ routerProducts.post(
     body.codigo = codigo;
     //El resto de los datos se suponen que se ingresan (nombre, precio, descripción, stock, foto)
     products.push(body);
+    write("./productos.json", products);
     res.json({ sucess: "ok", new: body });
   }
 );
@@ -118,19 +106,23 @@ routerProducts.put(
   (req, res) => {
     const { id } = req.params;
     const { body } = req;
-    product = products.find((x) => x.id == id);
+    const products = read("./productos.json");
+    const product = products.find((x) => x.id == id);
     if (product) {
       //Cambia el precio
       if (body.precio) {
         product.precio = body.precio;
+        write("./productos.json", products);
         res.json({ sucess: "ok", new: product });
       } //Cambia el nombre
       else if (body.nombre) {
         product.nombre = body.nombre;
+        write("./productos.json", products);
         res.json({ sucess: "ok", new: product });
       } //Cambia la foto
       else if (body.foto) {
         product.foto = body.foto;
+        write("./productos.json", products);
         res.json({ sucess: "ok", new: product });
       } //Cambia el id, timestamp, código...
       else if (body.id || body.timestamp || body.codigo) {
@@ -157,10 +149,12 @@ routerProducts.delete(
   },
   (req, res) => {
     const { id } = req.params;
-    product = products.find((x) => x.id == id);
+    let products = read("./productos.json");
+    const product = products.find((x) => x.id == id);
     if (product) {
       productsDelete = products.filter((x) => x.id != id);
       products = productsDelete;
+      write("./productos.json", products);
       res.json({ sucess: "ok", new: products });
     } else {
       res.json({ error: "producto no encontrado" });
@@ -168,63 +162,9 @@ routerProducts.delete(
   }
 );
 
-let carritos = [
-  {
-    id: 1,
-    timestamp: 1662007058984,
-    productos: [
-      {
-        id: 1,
-        timestamp: 1662007056984,
-        nombre: "remera",
-        descripcion: "descripción del producto",
-        codigo: "FYr3VGWDmyDnKKVVftG9",
-        precio: 500,
-        foto: "https://deliverind.com.ar/wp-content/uploads/2022/06/REMERA-REGULAR-FIT-SOFT-22-PORTADA-01-scaled.jpg",
-        stock: 1,
-      },
-      {
-        id: 3,
-        timestamp: 1662007058284,
-        nombre: "medias",
-        descripcion: "descripción del producto",
-        codigo: "62BPB72hk2jgGvM2ZM7r",
-        precio: 200,
-        foto: "https://deliverind.com.ar/wp-content/uploads/2021/09/4-10-scaled.jpg",
-        stock: 2,
-      },
-    ],
-  },
-  {
-    id: 2,
-    timestamp: 1662107059984,
-    productos: [
-      {
-        id: 2,
-        timestamp: 1662007058984,
-        nombre: "pantalon",
-        descripcion: "descripción del producto",
-        codigo: "wD2mPyXZW6R46nKgp9dJ",
-        precio: 800,
-        foto: "https://deliverind.com.ar/wp-content/uploads/2020/11/UNIDAD-71-scaled.jpg",
-        stock: 1,
-      },
-      {
-        id: 3,
-        timestamp: 1662007058284,
-        nombre: "medias",
-        descripcion: "descripción del producto",
-        codigo: "62BPB72hk2jgGvM2ZM7r",
-        precio: 200,
-        foto: "https://deliverind.com.ar/wp-content/uploads/2021/09/4-10-scaled.jpg",
-        stock: 1,
-      },
-    ],
-  },
-];
-
 routerCarrito.post("/", jsonParser, (req, res) => {
   const { body } = req;
+  const carritos = read("./carrito.json");
   //Asigno ID
   let lastID = carritos[carritos.length - 1].id;
   body.id = lastID + 1;
@@ -234,12 +174,14 @@ routerCarrito.post("/", jsonParser, (req, res) => {
   //
   //Los productos se supone que debería ingresarlos el cliente
   carritos.push(body);
-  res.json({ sucess: "ok", new: body });
+  write("./carrito.json", carritos);
+  res.json({ sucess: "ok", new: body.id });
 });
 
 routerCarrito.get("/:id/productos", (req, res) => {
   const { id } = req.params;
-  carrito = carritos.find((x) => x.id == id);
+  const carritos = read("./carrito.json");
+  const carrito = carritos.find((x) => x.id == id);
   if (carrito) {
     res.json(carrito.productos);
   } else {
@@ -249,14 +191,15 @@ routerCarrito.get("/:id/productos", (req, res) => {
 
 routerCarrito.delete("/:id", jsonParser, (req, res) => {
   const { id } = req.params;
-  carrito = carritos.find((x) => x.id == id);
+  let carritos = read("./carrito.json");
+  const carrito = carritos.find((x) => x.id == id);
   if (carrito) {
     //Vacio el carrito
     carrito.productos = [];
     //Borro Carrito
     carritoBorrado = carritos.filter((x) => x.id != id);
     carritos = carritoBorrado;
-
+    write("./carrito.json", carritos);
     res.json({ sucess: "ok", new: carritos });
   } else {
     res.json({ error: "carrito no encontrado" });
@@ -266,16 +209,19 @@ routerCarrito.delete("/:id", jsonParser, (req, res) => {
 routerCarrito.post("/:id/productos", jsonParser, (req, res) => {
   const { body } = req;
   const { id } = req.params;
+  const carritos = read("./carrito.json");
   const carrito = carritos.find((x) => x.id == id);
   if (carrito) {
     productoAgregado = carrito.productos.find((x) => x.id == body.id);
     if (productoAgregado) {
       //Si el producto ya estaba agregado al carrito modificamos su cantidad
       productoAgregado.stock = productoAgregado.stock + body.stock;
+      write("./carrito.json", carritos);
       res.json({ sucess: "ok", new: carrito });
     } else {
       //Si el producto no estaba en el carrito vamos agregarlo
       carrito.productos.push(body);
+      write("./carrito.json", carritos);
       res.json({ sucess: "ok", new: carrito });
     }
   } else {
@@ -286,6 +232,7 @@ routerCarrito.post("/:id/productos", jsonParser, (req, res) => {
 routerCarrito.delete("/:id/productos/:id_prod", jsonParser, (req, res) => {
   const { id } = req.params;
   const { id_prod } = req.params;
+  const carritos = read("./carrito.json");
   const carrito = carritos.find((x) => x.id == id);
   if (carrito) {
     productoEliminado = carrito.productos.find((x) => x.id == id_prod);
@@ -294,6 +241,7 @@ routerCarrito.delete("/:id/productos/:id_prod", jsonParser, (req, res) => {
         (x) => x.id != id_prod
       );
       carrito.productos = carritoSinProductoEliminado;
+      write("./carrito.json", carritos);
       res.json({ sucess: "ok", new: carrito });
     } else {
       res.json({ error: "producto no encontrado en el carrito" });
